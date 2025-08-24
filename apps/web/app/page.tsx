362cb851554fe376176f18bb8b913f92610fc1b0
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-08-24 16:18:15 +0300

    add connection to data base in both  web and widget app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,16 @@
-import { Button } from "@workspace/ui/components/button";
-import { add } from "@workspace/math/add";
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Apps Web</h1>
-        <Button size="sm">{"Click"}</Button>
-        <p>{add(2, 3)}</p>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <h1 className="text-2xl font-bold">Hello Apps Web</h1>
+      <div className="max-w-sm w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
       </div>
     </div>
   );

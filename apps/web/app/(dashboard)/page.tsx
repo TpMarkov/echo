025e10cc2e4b4b70b2ca82824bd8210922734414
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-08-26 14:34:07 +0300

    05:Error-tracking

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -3,6 +3,7 @@
 import { useMutation, useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { UserButton, OrganizationSwitcher } from "@clerk/nextjs";
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
   const users = useQuery(api.users.getMany) || [];
@@ -12,8 +13,10 @@ export default function Page() {
     <>
       <div className="flex flex-col items-center justify-center min-h-svh">
         <p>Apps / Web</p>
+
         <UserButton></UserButton>
         <OrganizationSwitcher hidePersonal />
+        <Button onClick={() => addUser()}>Add User</Button>
       </div>
     </>
   );
